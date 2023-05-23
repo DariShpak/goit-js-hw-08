130c3a31d4665bfbfa16405a1486b561ce2312c4
Daria Shpak <darya.shpak@gmail.com>
2023-05-23 18:59:21 +0300

    first commit/second try

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,3 +6,28 @@ console.log(galleryItems);
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
+const gallery = document.querySelector('.gallery');
+
+(function () {
+  const markupGallery = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" 
+     alt="${description}"/>
+   </a>
+  </li>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markupGallery);
+})();
+
+const lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
