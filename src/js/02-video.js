4dbb0450a0fa39727381a499731e30ffca66aa04
Daria Shpak <darya.shpak@gmail.com>
2023-05-24 02:23:50 +0300

    Task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,37 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-console.log(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+player.on('timeupdate', throttle(currTime, 1000));
+
+function currTime(currentTime) {
+  save(LOCALSTORAGE_KEY, currentTime.seconds);
+}
+
+window.addEventListener('load', event => {
+  player.setCurrentTime(load(LOCALSTORAGE_KEY) || 0);
+});
